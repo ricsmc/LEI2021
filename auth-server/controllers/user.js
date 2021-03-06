d87f34c3fe68d02dfd2d75a2b16e77b9c6a7cd72
Ricardo Cunha <ricardosmcunha23@gmail.com>
2021-03-06 18:22:47 +0000

    first commit

---
+++ auth-server/controllers/user.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose')
+var User = require('../models/user')
+
+// Listar os users
+module.exports.list = ()=> {
+    return User.find().exec()
+}
+
+// Procurar o user id
+module.exports.lookUp = id => {
+    return User.findOne({username: id}).exec()
+}
+
+// Inserir o user u
+module.exports.insert = u => {
+    console.log(JSON.stringify(u))
+    var newUser = new User(u)
+    return newUser.save()
+}
+
+// Remover o user id
+module.exports.remove = id => {
+    return User.deleteOne({_id : id})
+}
+
+// Editar o user id para u
+module.exports.edit = (id,u) => {
+    return User.findOneAndUpdate({username:id}, u, {new: true})
+}

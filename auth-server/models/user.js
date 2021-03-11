6a407badc8f0ad7be6925731b2e722b73cb14046
Ricardo Cunha <ricardosmcunha23@gmail.com>
2021-03-11 18:44:17 +0000

    api_completa
    Update dos campos da api

--- auth-server/models/user.js
+++ auth-server/models/user.js
@@ -4,9 +4,13 @@ var mongoose = require('mongoose')
 var userSchema = new mongoose.Schema({
     username: { type:  String, required: true },
     email: { type:  String, required: true },
-    password: { type:  String, required: true }
+    password: { type:  String, required: true },
+    createdAt: {type: Date, default: Date.now},
+    published_at: {type: Date, default: Date.now},
+    updatedAt: {type: Date, default: Date.now},
+    date_of_registry: {type: Date, default: Date.now}
     
 })
 
 
-module.exports = mongoose.model('utilizador' , userSchema)
+module.exports = mongoose.model('utilizadore' , userSchema)

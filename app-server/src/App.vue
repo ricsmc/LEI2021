7669b752d1c3beab89c75a70ad929d9dc7246a32
LuisRamos99 <48031198+LuisRamos99@users.noreply.github.com>
2021-03-26 20:59:01 +0000

    logout and icons
    logout e icons

--- app-server/src/App.vue
+++ app-server/src/App.vue
@@ -8,12 +8,22 @@
       <a href="/" class="logo"><b>memorybook</b></a>
       <v-spacer/>
       
-      <div v-if="this.token">
-        <h1>{{data}}
-        <v-avatar size="50">
-          <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
-        </v-avatar>
-        </h1>
+      <div v-if="this.token" class="dropdown">
+        <div class="dropbtn">
+          <b class="name">{{data}}</b>
+          <v-avatar size="50">
+            <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
+          </v-avatar>
+          <i class="fa fa-sort-desc" style="margin-left:10px"></i>
+        </div>
+        <div class="dropdown-content">
+          <a href="/">Link 1</a>
+          <a href="/">Link 2</a>
+          <a v-on:click="handleLogout()" href="/">
+            <i style="margin-right:2px" class="fa fa-sign-out" aria-hidden="true"></i>
+            Logout
+          </a>
+        </div>
       </div>
       <div v-else>
           <Login></Login>
@@ -70,10 +80,11 @@ export default {
             return {
                     data:"",
                     show_login:false,
-                    token:localStorage.getItem('jwt')
+                    token:false
             }
         },
   created() {
+    this.token=localStorage.getItem('jwt'),
     this.data=localStorage.getItem('user')
   },
   components: {
@@ -91,6 +102,9 @@ export default {
       else this.show_login=true
       console.log(this.show_login)
     },
+    handleLogout() {
+       localStorage.clear();
+    }
   }
 
 };
@@ -128,6 +142,56 @@ export default {
   .logo:hover{
     -webkit-text-fill-color: white;
   }
+
+  .dropdown {
+    float: left;
+    overflow: hidden;
+  }
+
+  .dropdown .dropbtn {
+  font-size: 16px;  
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+  }
+     
+  .dropdown-content {
+    display: none;
+    position: absolute;
+    right: 10px;
+    background-color: #f9f9f9;
+    min-width: 150px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+  }
+  
+  .dropdown-content a {
+    float: none;
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+  }
+
+.dropdown-content a:hover {
+  background-color: #ddd;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+.name {
+  margin-right:10px;
+  font-size:24px
+}
+
+
 </style>
 
 

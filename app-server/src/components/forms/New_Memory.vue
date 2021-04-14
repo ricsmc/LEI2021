fdccb1ee94478de4e2ee059c6d9ce96570d667b8
LuisRamos99 <48031198+LuisRamos99@users.noreply.github.com>
2021-04-14 14:19:59 +0100

    start of people in memory

--- app-server/src/components/forms/New_Memory.vue
+++ app-server/src/components/forms/New_Memory.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="new_memory">
+  <div id="new_memory" >
     <span class="headline">
         <h2 style="padding: 20px 0px 0px 0px; color:#4F4E81">
         Nova Memória
@@ -13,36 +13,55 @@
             <v-textarea outlined auto-grow type="text" :rules="rules" v-model="content" label="Corpo *"></v-textarea>
         </v-col>
         <v-row no-gutters>
-            <v-col cols="12" sm="6">
+            <v-col sm="2" >
                 <v-text-field type="text" prepend-icon="mdi-map-marker" v-model="local" label="Local"></v-text-field>
             </v-col>
-            <v-menu
-              ref="menu"
-              v-model="menu"
-              :close-on-content-click="false"
-              transition="scale-transition"
-              offset-y
-              min-width="auto"
-            >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
+            <v-col sm="2" offset=1>
+              <v-menu
+                ref="menu"
+                v-model="menu"
+                :close-on-content-click="false"
+                transition="scale-transition"
+                offset-y
+                min-width="auto"
+              >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="date"
+                      label="Data"
+                      prepend-icon="mdi-calendar"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker
+                    ref="picker"
                     v-model="date"
-                    label="Data"
-                    prepend-icon="mdi-calendar"
-                    readonly
-                    v-bind="attrs"
-                    v-on="on"
-                  ></v-text-field>
-                </template>
-                <v-date-picker
-                  ref="picker"
-                  v-model="date"
-                  color="#4F4E81"
-                  :max="new Date().toISOString().substr(0, 10)"
-                  min="1900-01-01"
-                  @change="save"
-                ></v-date-picker>
-            </v-menu>
+                    color="#4F4E81"
+                    :max="new Date().toISOString().substr(0, 10)"
+                    min="1900-01-01"
+                    @change="save"
+                  ></v-date-picker>
+              </v-menu>
+            </v-col>
+            <v-col sm="4" offset=1>
+              <v-autocomplete
+                v-model="values"
+                :items="people"
+                item-text="name"
+                outlined
+                dense
+                chips
+                small-chips
+                label="Escolhe as personagens"
+                return-object
+                multiple
+              ></v-autocomplete>
+            </v-col>
+            <v-col sm="1">
+              <v-btn dark elevation="1" v-on:click="newPerson()" style="margin-left:15px"> Criar nova </v-btn>
+            </v-col>
         </v-row>
         <v-row>
           <v-container>
@@ -54,15 +73,15 @@
 </template>
 
 
-
-
 <script>
 import axios from 'axios'
-    export default {
+import gql from 'graphql-tag'
 
+    export default {
         name: 'New_Memory',
         data() {
             return {
+                values: [],
                 date: null,
                 menu: false,
                 title: "",
@@ -74,39 +93,49 @@ import axios from 'axios'
             }
         },
         watch: {
-    menu (val) {
-      val && setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'))
-    },
-  },
-  methods: {
-    save (date) {
-      this.$refs.menu.save(date)
-    },
-    post(){
-      console.log(this.date)
-      console.log(this.title)
-      console.log(this.content)
-      console.log(this.local)
-      var json = {}
-      json['utilizador'] = localStorage.getItem('id')
-      json['title']    = this.title
-      json['content'] = this.content
-      json['local']    = this.local
-      json['date_of_memory'] = this.date
-      var token = localStorage.getItem('jwt')
-      console.log(json)
-      axios.post("http://localhost:1337/memories",  json,{headers: {'Authorization': `${token}`}})
-        .then(data => {
-          this.$router.push('/memories/' + data.data.id)
-        })
-        .catch(err => {
-          console.log(err)
-        })
-    }
+          menu (val) {
+            val && setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'))
+          },
         },
-
-
+        methods: {
+          save (date) {
+            this.$refs.menu.save(date)
+          },
+          post(){
+            var json = {}
+            json['utilizador'] = localStorage.getItem('id')
+            json['title'] = this.title
+            json['content'] = this.content
+            json['local'] = this.local
+            json['date_of_memory'] = this.date
+            var token = localStorage.getItem('jwt')
+            axios.post("http://localhost:1337/memories",  json,{headers: {'Authorization': `${token}`}})
+              .then(data => {
+                this.$router.push('/memories/' + data.data.id)
+                this.$router.go()
+              })
+              .catch(err => {
+                console.log(err)
+              })
+          }
+        },
+        apollo: {
+          people: {
+            query: gql`
+              query People ($id :ID!) {
+                people(where:{ memories : {utilizador: {id:$id}}}) {
+                  id
+                  name
+                }
+              }
+            `,
+            variables: {
+              id: localStorage.getItem('id')
+            }
+          }
+        }      
     }
+
 </script>
 
 <style>

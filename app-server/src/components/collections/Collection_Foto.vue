c5b64f3211234c04e0adad9acdbb780a0dc48318
LuisRamos99 <48031198+LuisRamos99@users.noreply.github.com>
2021-04-25 18:50:40 +0100

    New

---
+++ app-server/src/components/collections/Collection_Foto.vue
@@ -0,0 +1,37 @@
+<template> 
+    <div id="collection_foto">
+         
+        <div class="cardd" v-if="urls.length>=4">
+              <v-row>
+                  <img :src="`http://localhost:1337`+this.urls[0]"/>
+                  <img :src="`http://localhost:1337`+this.urls[1]"/>
+              </v-row>
+              <v-row>
+                  <img :src="`http://localhost:1337`+this.urls[2]"/>
+                  <img :src="`http://localhost:1337`+this.urls[3]"/>
+              </v-row>
+        </div> 
+
+        <div class="carddAlone" v-else-if="urls.length>0">
+          <img :src="`http://localhost:1337`+this.urls[0]"/>
+        </div>
+
+        <div class="carddAlone" v-else>
+          <img src="https://atasouthport.com/wp-content/uploads/2017/04/default-image-620x600.jpg"/>
+        </div>
+
+    </div>
+</template>
+
+
+
+<script>
+
+export default {
+    name: 'collection_foto',
+    props: {
+        urls : Array
+    }
+}    
+
+</script>
